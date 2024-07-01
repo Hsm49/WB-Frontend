3fcc8662f2380efa441338ac1d4b4935b4f8f55f
JVRALPH <004ralph@gmail.com>
2024-07-01 01:52:49 -0600

    Revert "homepage"
    This reverts commit d93ab484dd6e8b673e79b1320a2b96c26d4bdd77.

--- src/app/components/authetication/login/login.component.ts
+++ src/app/components/authetication/login/login.component.ts
@@ -3,49 +3,45 @@ import { Router } from '@angular/router';
 import { Userdto } from 'src/app/common/userdto';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { SessionStorageService } from 'src/app/services/session-storage.service';
-import jwt_decode from 'jwt-decode'; // Importa jwt-decode
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  username: string = '';
-  password: string = '';
+export class LoginComponent implements OnInit{
+  username : string = '';
+  password : string = '';
 
-  constructor(
-    private authentication: AuthenticationService,
-    private sessionStorage: SessionStorageService,
+
+  ngOnInit(): void {
+
+  }
+
+  constructor(private authentication : AuthenticationService, 
+    private sessionStorage : SessionStorageService,
     private router: Router
-  ) {}
+    ){
 
-  ngOnInit(): void {}
+  }
 
-  login() {
+  login(){
     let userDto = new Userdto(this.username, this.password);
     this.authentication.login(userDto).subscribe(
       token => {
-        console.log('Token recibido:', token);
-
-        // Decodificar el token para obtener los datos del usuario
-        let decodedToken = jwt_decode(token);
-        console.log('Token decodificado:', decodedToken);
-
-        // Verificar el tipo de usuario
-        if (decodedToken && decodedToken.authorities.includes('ROLE_ADMIN')) {
+        console.log(token);
+        //this.sessionStorage.removeItem('token');
+        this.sessionStorage.setItem('token', token);
+        if(token.type == 'ADMIN'){
           this.router.navigate(['/admin/product']);
-        } else {
+        }else{
           this.router.navigate(['/']);
         }
-
-        // Almacena el token en el almacenamiento de sesión
-        this.sessionStorage.setItem('token', token);
-      },
-      error => {
-        console.error('Error durante el inicio de sesión:', error);
-        // Manejar errores de autenticación aquí (por ejemplo, mostrar un mensaje de error al usuario)
       }
     );
+    console.log(userDto);
   }
+
+
+
 }
